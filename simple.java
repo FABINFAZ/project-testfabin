49eeeb54c256af8f185418e3a4cb89da17393150
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-12-19 23:07:39 -0500

    second commit of simple.java

--- simple.java
+++ simple.java
@@ -3,3 +3,8 @@ class simple{
 		system.out.println("Hello edurekha,Welcome to gits online training")
 	}
 }
+class java{
+	public stat void main string args[]{
+		system.out.println("welcome to devops course")
+	}
+}
